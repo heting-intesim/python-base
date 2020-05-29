6403911dd89602bbd1da293f11167e212ddc3941
heting <heting@123.com>
2020-05-29 16:56:46 +0800

    change office

--- 基础语法/for-else.py
+++ 基础语法/for-else.py
@@ -9,3 +9,15 @@ for i in p:
 else: # 只有当for顺利执行完毕，而没有break时，才会执行else 之后的语句！  
     print('---此批产品合格！---') # 一旦break，本句就不执行
 print('End')
+
+#登录验证
+for i in range(3):
+    name = input('请输入用户名：')
+    password = input('请输入密码：')
+    if name!='zhao' or password!='123':
+        print('输入错误！')
+        continue
+    print('欢迎 zhao')
+    break
+else:     #此句之后的语句只有当for结构不在循环中break，才会执行；  一旦break此句不执行！！
+    print('输入错次数已达上限，强制退出')
