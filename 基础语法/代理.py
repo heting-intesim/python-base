6a3b19f7dfd1a137a2baf78c9d7975127d08969b
tingdang-home <tingdang@123.com>
2020-06-23 20:19:28 +0800

    home

---
+++ 基础语法/代理.py
@@ -0,0 +1,15 @@
+import urllib.request,urllib.parse
+
+# 高级功能首先创建handler
+handler = urllib.request.ProxyHandler({'http':'163.125.223.102:8088',})
+# 创建opener
+opener = urllib.request.build_opener(handler)
+
+url = 'https://www.baidu.com/s?ie=UTF-8&wd=ip'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+}
+request = urllib.request.Request(url,headers=headers)
+response = opener.open(request)
+with open('1.html','wb') as f:
+    f.write(response.read())
--- 基础语法/皇后.py
+++ 基础语法/皇后.py
@@ -1,3 +1,4 @@
+import time
 # 打印棋盘
 def show(data):
     n = len(data)
@@ -25,15 +26,67 @@ def huang(m,n,b):
             else:
                 continue
 
-res = []
-huang(5,5,[])
-i_res = iter(res)
-while True:
-    if input('显示下一个布局？[按回车继续][按Ctrl+c结束]') == '':
-        try:
-            r = next(i_res)
-            print(r)
-            show(r)
-        except:
-            print(f'已全部显示完毕，共 {len(res)} 种布局方案！')
-            break
+# res = []
+# # t1 = time.time()
+# huang(4,4,[])
+# t2 = time.time()
+# print(res,'总数：',len(res))
+# print('runnint TIME:',t2-t1)
+
+
+def huang2(m,n,b):
+    global sum
+    if m == 1:
+        for i in range(n):
+            for k,v in enumerate(b):
+                if i == v or abs(k-len(b)) == abs(v-i):
+                    break
+            else:
+                # b.append(i)
+                # print(b)
+                # res.append(b)
+                sum += 1
+    else:
+        for j in range(n):
+            if len(b) == 0:
+                c = b + [j]
+                huang2(m-1,n,c)
+            else:
+                for k,v in enumerate(b):
+                    if j == v or abs(k-len(b)) == abs(v-j):
+                        break
+                else:
+                    c = b + [j]
+                    huang2(m-1,n,c)
+# res = []
+# sum = 0
+# n = 15
+# huang2(n,n,[]) 
+# print(sum)
+# print(f'{n}皇后问题存在{len(res)}种布局方案')
+# # 用迭代器逐个显示棋盘
+# i_res = iter(res)
+# while True:
+#     if input('显示下一个布局？[按回车继续][按Ctrl+c结束]') == '':
+#         try:
+#             r = next(i_res)
+#             print(r)
+#             show(r)
+#         except:
+#             print(f'已全部显示完毕，共 {len(res)} 种布局方案！')
+#             break
+
+# 标准答案
+def queen(A, cur=0):
+    if cur == len(A):
+        print(A)
+        return 0
+    for col in range(len(A)):
+        A[cur], flag = col, True
+        for row in range(cur):
+            if A[row] == col or abs(col - A[row]) == cur - row:
+                flag = False
+                break
+        if flag:
+            queen(A, cur+1)
+queen([None]*8)
